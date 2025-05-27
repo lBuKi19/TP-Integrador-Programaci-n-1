6bc77d9311745213182642594e8674756599c248
Lautaro B <lautabuchek@gmail.com>
2025-05-27 11:37:11 -0700

    Creación de TP integrador

---
+++ TP INTEGRADOR PROGRAMACIÓN 1.py
@@ -0,0 +1,19 @@
+#TP INTEGRADOR PROGRAMACIÓN 1
+
+print("Algoritmos de ordenamiento:\n"
+      "1. Bubble Sort\n"
+      "1. Merge Sort\n"
+      "1. Quick Sort\n"
+      "1. Insertion Sort\n"
+      "1. Selection Sort\n")
+opcion_ordenamiento = int(input("Ingrese el número de algoritmo de ordenamiento que desea ejecutar:"))
+
+#SE REALIZA EL SORT ESPECIFICADO EN EL CSV
+
+#Búsqueda
+print("Algoritmos de ordenamiento:\n"
+      "1. Binary Search\n"
+      "1. Linear Search\n")
+      
+opcion_busqueda = int(input("Ingrese el número de algoritmo de búsqueda que desea ejecutar:"))
+#SE REALIZA EL SEARCH ESPECIFICADO EN EL CSV
---
+++ funciones_integrador.py
@@ -0,0 +1,24 @@
+#Definición de funciones de algoritmos 
+
+#Algoritmos de ordenamiento
+def bubble_sort(CSV):
+    return 1
+
+def merge_sort(CSV):
+    return 1
+
+def quick_sort(CSV):
+    return 1
+
+def selection_sort(CSV):
+    return 1
+
+def insertion_sort(CSV):
+    return 1
+
+#Algoritmos de búsqueda
+def linear_search(CSV):
+    return 1
+
+def binary_search(CSV):
+    return 1
